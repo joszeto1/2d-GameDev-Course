3916803803868ce61e8a975a1eb2d4a38611a3a1
joszeto1 <joszeto1@s.sfusd.edu>
2024-03-26 14:00:03 -0700

    L5. Making the ship move smoothly with steering and practice

--- 04.to_space_and_beyond_workbook/lessons/ship.gd
+++ 04.to_space_and_beyond_workbook/lessons/ship.gd
@@ -1,9 +1,11 @@
 extends Sprite2D
 
-var velocity = Vector2(0,0)
-var max_speed = 600
 var boost_speed = 1500
 var normal_speed = 600
+
+var max_speed = normal_speed
+var velocity = Vector2(0,0)
+var steering_factor = 8
 
 func _process(delta):
 	var direction = Vector2(0,0)
@@ -16,8 +18,10 @@ func _process(delta):
 	if Input.is_action_just_pressed("boost"):
 		max_speed = boost_speed
 		get_node("Timer").start()
-	
-	velocity = direction * max_speed
+		
+	var desired_velocity = max_speed * direction
+	var steering_vector = desired_velocity - velocity
+	velocity += steering_vector * steering_factor * delta
 	position += velocity * delta
 	
 	if direction.length() > 0:
