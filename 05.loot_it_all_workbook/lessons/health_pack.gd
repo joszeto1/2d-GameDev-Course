9c1912c8b8dc20d9ae6b99449041411edf8b6e6b
joszeto1 <joszeto1@s.sfusd.edu>
2024-04-02 14:09:13 -0700

    L3. Make the ship Interact

--- 05.loot_it_all_workbook/lessons/health_pack.gd
+++ 05.loot_it_all_workbook/lessons/health_pack.gd
@@ -1,7 +1,8 @@
 extends Area2D
 
-func _on_area_entered(area_that_entered: Area2D):
-	queue_free()
-	
+
 func _ready():
 	area_entered.connect(_on_area_entered)
+
+func _on_area_entered(area_that_entered: Area2D):
+	queue_free()
