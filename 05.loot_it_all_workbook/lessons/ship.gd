a4b5a3d71e4bc4b5410b1465161729cc1e741d7b
joszeto1 <joszeto1@s.sfusd.edu>
2024-04-15 14:45:24 -0700

    L6. Add Collectible Gems

--- 05.loot_it_all_workbook/lessons/ship.gd
+++ 05.loot_it_all_workbook/lessons/ship.gd
@@ -1,9 +1,10 @@
 extends Area2D
 
-var health = 10
+var health: = 10
 var max_speed := 1200.0
 var velocity := Vector2(0, 0)
-var steering_factor := 3.0
+var steering_factor := 8
+var gem_count: = 0
 
 func _ready():
 	area_entered.connect(_on_area_entered)
@@ -31,5 +32,12 @@ func set_health(new_health):
 	health = new_health
 	get_node("UI/HealthBar").value = health
 	
+func set_gem_count(new_gem_count: int):
+	gem_count = new_gem_count
+	get_node("UI/GemCount").text = "x" + str(gem_count)
+	
 func _on_area_entered(area_that_entered: Area2D):
-	set_health(health + 10)
+	if area_that_entered.is_in_group("gem"):
+		set_gem_count(gem_count + 1)
+	elif area_that_entered.is_in_group("healing_item"):
+		set_health(health + 10)
