f188c337b3972d0f40a55cb2c6b338ddd8bba4f1
joszeto1 <joszeto1@s.sfusd.edu>
2024-04-23 14:22:46 -0700

    L3. the outline thickens

---
+++ M06.looting_workbook/lessons/chest/chest.gd
@@ -0,0 +1,21 @@
+extends Area2D
+
+@onready var canvas_group: CanvasGroup = $CanvasGroup
+
+
+func _ready():
+	mouse_entered.connect(_on_mouse_entered)
+	mouse_exited.connect(_on_mouse_exited)
+	canvas_group.material.set_shader_parameter("line_thickness",3)
+	
+func _on_mouse_entered():
+	var tween = create_tween()
+	tween.tween_method(set_outline_thickness,3,6,0.08)
+	
+func _on_mouse_exited():
+	var tween = create_tween()
+	tween.tween_method(set_outline_thickness,6,3,0.08)
+	
+
+func set_outline_thickness(new_thickness):
+	canvas_group.material.set_shader_parameter("line_thickness",new_thickness)
