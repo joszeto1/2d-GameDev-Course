20a019b01efad389ef48f0d23b2dd05a7877fc14
joszeto1 <joszeto1@s.sfusd.edu>
2024-04-30 14:08:46 -0700

    L7. animate the collectibles

--- M06.looting_workbook/lessons/chest/chest.gd
+++ M06.looting_workbook/lessons/chest/chest.gd
@@ -48,4 +48,21 @@ func _spawn_random_item():
 	var random_angle = randf_range(0, 2 * PI)
 	var random_direction = Vector2(1,0).rotated(random_angle)
 	var random_distance = randf_range(60, 120)
-	loot_item.position = random_direction * random_distance
+	var land_position = random_direction * random_distance
+	
+	const FLIGHT_TIME = 0.4
+	const HALF_FLIGHT_TIME = FLIGHT_TIME/2
+	
+	var tween = create_tween()
+	tween.set_parallel()
+	loot_item.scale = Vector2(0.25,0.25)
+	tween.tween_property(loot_item, "scale", Vector2(1,1), HALF_FLIGHT_TIME)
+	tween.tween_property(loot_item, "position:x", land_position.x, FLIGHT_TIME)
+
+	tween = create_tween()
+	tween.set_trans(Tween.TRANS_QUAD)
+	tween.set_ease(Tween.EASE_OUT)
+	var jump_height = randf_range(30, 80)
+	tween.tween_property(loot_item, "position:y", land_position.y - jump_height, HALF_FLIGHT_TIME)
+	tween.set_ease(Tween.EASE_IN)
+	tween.tween_property(loot_item, "position:y", land_position.y, HALF_FLIGHT_TIME)
